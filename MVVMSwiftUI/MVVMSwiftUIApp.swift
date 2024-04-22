815ad236a29ebd6af73d12858ef18e655c7ad896
DuyenNTM <dyndyn2602@gmail.com>
2024-04-22 09:00:23 +0700

    Initial Commit

---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Din Din on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
